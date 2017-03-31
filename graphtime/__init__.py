e292d9dc99a44e52a9e10ca86b5354af21e440ec
Alexander Immer <alexander.immer@yahoo.com>
2017-03-31 16:45:04 +0200

    Add initial package version

--- graphtime/__init__.py
+++ graphtime/__init__.py
@@ -1,2 +1,5 @@
 from graphtime.graphtime import *
 from graphtime.utils import *
+
+__version__ = (0, 1, 0)
+
---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+current_version = '0.1'
+
+setup(
+    name='graphtime',
+    packages=['graphtime'],
+    version=current_version,
+    description='Dynamical Graphical Model Estimation',
+    author='Glooper Limited',
+    author_email='alex.gibberd@glooper.io, alex.immer@glooper.io',
+    url='https://github.com/GlooperLabs/GraphTime',
+    download_url='https://github.com/AlexImmer/GlooperLabs/GraphTime/v'
+                + current_version + '.tar.gz',
+    keywords=['dynamic', 'graphical models', 'time series', 'non-stationary'],
+    classifiers=[]
+)
